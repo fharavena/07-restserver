433c0bfd27659ff75432c7b1b30cea2c1fce4b79
fharavena <fharavena@gmail.com>
2019-01-27 23:21:27 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:Conejo123_@ds213705.mlab.com:13705/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
